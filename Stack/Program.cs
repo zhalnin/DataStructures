60095192806b94687ea94dde0fe930873078e0a1
Aleksey Zhalnin <zhalninreg@me.com>
2023-06-27 05:21:55 +0300

    Added LinkedStack type

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Stack.Models;
+
 Console.WriteLine("Hello, Stack!");
+
+var easyStack = new EasyStack<int>();
+easyStack.Push(1);
+easyStack.Push(2);
+easyStack.Push(3);
+
+var item =  easyStack.Pop();
+var item2 = easyStack.Peek();
+
+Console.WriteLine(item);
+Console.WriteLine(item2);
+
+Console.ReadLine();
