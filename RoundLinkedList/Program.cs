7c39d557c046aad6edec0c7d03787dc6fa6cd086
Aleksey Zhalnin <zhalninreg@me.com>
2023-06-30 20:34:39 +0300

    Added duplex link list

---
+++ RoundLinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RoundLinkedList.Models;
+
+Console.WriteLine("Hello, World!");
+
+var duplexList = new DuplexLinkedList<int>();
+duplexList.Add(1);
+duplexList.Add(2);
+duplexList.Add(3);
+duplexList.Add(4);
+duplexList.Add(5);
+
+foreach(var duplex in duplexList)
+{
+    Console.WriteLine(duplex);
+}
+Console.WriteLine("----------------");
+duplexList.Delete(2);
+
+foreach (var duplex in duplexList)
+{
+    Console.WriteLine(duplex);
+}
+Console.WriteLine("----------------");
+var reverse = duplexList.Reverse();
+
+foreach (var duplex in reverse)
+{
+    Console.WriteLine(duplex);
+}
+Console.WriteLine("----------------");
