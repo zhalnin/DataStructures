77b8166da306ee4fefaf2d553bcbc8d9fdb72e6b
Aleksey Zhalnin <zhalninreg@me.com>
2023-07-07 18:39:54 +0300

    Added array based queue

--- Queue/Models/Queue.cs
+++ Queue/Models/Queue.cs
@@ -1,43 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Queue.Models;
 
-namespace Queue.Models
+public  class Queue<T>
 {
-    public  class Queue<T>
+    private readonly List<T> _items = new List<T>();
+    private T Head => _items.Last();
+    private T Tail => _items.First();
+    public int Count => _items.Count;
+
+    public Queue()
     {
-        private readonly List<T> _items = new List<T>();
-        private T Head => _items.Last();
-        private T Tail => _items.First();
-        public int Count => _items.Count;
+        
+    }
 
-        public Queue()
-        {
-            
-        }
+    public Queue(T data)
+    {
+        _items.Add(data);
+    }
 
-        public Queue(T data)
-        {
-            _items.Add(data);
-        }
+    public void Enqueue(T data)
+    {
+        _items.Insert(0, data);
+    }
 
-        public void Enqueue(T data)
-        {
-            _items.Insert(0, data);
-        }
+    public T Dequeue()
+    {
+        var item = Head;
+        _items.Remove(item);
+        return item;
+    }
 
-        public T Dequeue()
-        {
-            var item = Head;
-            _items.Remove(item);
-            return item;
-        }
-
-        public T Peek()
-        {
-            return Head;
-        }
+    public T Peek()
+    {
+        return Head;
     }
 }
--- Queue/Program.cs
+++ Queue/Program.cs
@@ -1,13 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Queue.Models;
+
 Console.WriteLine("Hello, World!");
 
-var queue = new Queue<int>();
-queue.Enqueue(1);
-queue.Enqueue(2);
-queue.Enqueue(3);
-queue.Enqueue(4);
-queue.Enqueue(5);
+//var queue = new Queue<int>();
+//queue.Enqueue(1);
+//queue.Enqueue(2);
+//queue.Enqueue(3);
+//queue.Enqueue(4);
+//queue.Enqueue(5);
 
-Console.WriteLine(queue.Dequeue());
-Console.WriteLine(queue.Peek());
-Console.WriteLine(queue.Dequeue());
+//Console.WriteLine(queue.Dequeue());
+//Console.WriteLine(queue.Peek());
+//Console.WriteLine(queue.Dequeue());
+
+var arrayQueue = new ArrayQueue<int>(5);
+arrayQueue.Enqueue(1);
+arrayQueue.Enqueue(2);
+arrayQueue.Enqueue(3);
+arrayQueue.Enqueue(4);
+arrayQueue.Enqueue(5);
+
+Console.WriteLine(arrayQueue.Dequeue());
+Console.WriteLine(arrayQueue.Peek());
+Console.WriteLine(arrayQueue.Dequeue());
