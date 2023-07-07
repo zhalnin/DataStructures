5954d7cba208594925225cb545f2ff5118a6ecbe
Aleksey Zhalnin <zhalninreg@me.com>
2023-07-07 17:09:11 +0300

    Added list based queue

---
+++ Queue/Models/Queue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue.Models
+{
+    public  class Queue<T>
+    {
+        private readonly List<T> _items = new List<T>();
+        private T Head => _items.Last();
+        private T Tail => _items.First();
+        public int Count => _items.Count;
+
+        public Queue()
+        {
+            
+        }
+
+        public Queue(T data)
+        {
+            _items.Add(data);
+        }
+
+        public void Enqueue(T data)
+        {
+            _items.Insert(0, data);
+        }
+
+        public T Dequeue()
+        {
+            var item = Head;
+            _items.Remove(item);
+            return item;
+        }
+
+        public T Peek()
+        {
+            return Head;
+        }
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var queue = new Queue<int>();
+queue.Enqueue(1);
+queue.Enqueue(2);
+queue.Enqueue(3);
+queue.Enqueue(4);
+queue.Enqueue(5);
+
+Console.WriteLine(queue.Dequeue());
+Console.WriteLine(queue.Peek());
+Console.WriteLine(queue.Dequeue());
