8c16c9c26c0710d54ab655b50d837ad5f8a60313
Aleksey Zhalnin <zhalninreg@me.com>
2023-07-16 19:28:12 +0300

    Added better hash table

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -10,3 +10,11 @@ easyHashTable.Add(777);
 
 Console.WriteLine(easyHashTable.Search(6));
 Console.WriteLine(easyHashTable.Search(18));
+
+HashTable<int, string> hashTable = new(100);
+hashTable.Add(5,"Hello");
+hashTable.Add(18,"World");
+hashTable.Add(777,"Bro");
+
+Console.WriteLine(hashTable.Search(6, "Peter"));
+Console.WriteLine(hashTable.Search(18, "World"));
