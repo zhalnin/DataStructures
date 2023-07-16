d2e785f400719f01250ecd652bc2a526f97b13d9
Aleksey Zhalnin <zhalninreg@me.com>
2023-07-16 19:10:49 +0300

    It is easy hash table

---
+++ HashTable/Models/EasyHashTable.cs
@@ -0,0 +1,28 @@
+﻿namespace HashTable.Models;
+
+internal class EasyHashTable<T>
+{
+    private T[] _items;
+
+    public EasyHashTable(int size)
+    {
+        _items = new T[size];
+    }
+
+    public void Add(T item)
+    {
+        var key = GetHash(item);
+        _items[key] = item;
+    }
+
+    public bool Search(T  item)
+    {
+        var key = GetHash(item);
+        return _items[key].Equals(item);
+    }
+
+    private int GetHash(T item)
+    {
+        return item.ToString().Length % _items.Length;
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HashTable.Models;
+
+Console.WriteLine("Hello, HashTable!");
+
+EasyHashTable<int> easyHashTable = new(100);
+easyHashTable.Add(5);
+easyHashTable.Add(18);
+easyHashTable.Add(777);
+
+Console.WriteLine(easyHashTable.Search(6));
+Console.WriteLine(easyHashTable.Search(18));
