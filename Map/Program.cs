e6979c054f7f872ccd51ba614e7f7f88f60c87ea
Aleksey Zhalnin <zhalninreg@me.com>
2023-07-22 21:35:33 +0300

    Added easyMap and map

---
+++ Map/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Map.Models;
+
+Console.WriteLine("Hello, Map!");
+
+var easyMap = new EasyMap<int, string>
+{
+    new Item<int, string>(1, "One"),
+    new Item<int, string>(2, "Two"),
+    new Item<int, string>(3, "Three"),
+    new Item<int, string>(4, "Four"),
+    new Item<int, string>(5, "Five")
+};
+
+foreach (var item in easyMap)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine(easyMap.Search(7) ?? "Not found");
+Console.WriteLine(easyMap.Search(3) ?? "Not found");
+
+easyMap.Remove(3);
+easyMap.Remove(1);
+
+foreach (var item in easyMap)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("----------------");
+ 
+var dict = new Dict<int, string>
+{
+    new Item<int, string>(1, "One#1"),
+    new Item<int, string>(1, "One#2"),
+    new Item<int, string>(2, "Two"),
+    new Item<int, string>(3, "Three"),
+    new Item<int, string>(4, "Four"),
+    new Item<int, string>(5, "Five"),
+    new Item<int, string>(101, "Hundred")
+};
+
+foreach (var item in dict)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine(dict.Search(7) ?? "Not found");
+Console.WriteLine(dict.Search(3) ?? "Not found");
+Console.WriteLine(dict.Search(101) ?? "Not found");
+
+dict.Remove(3);
+dict.Remove(1);
+dict.Remove(101);
+
+foreach (var item in dict)
+{
+    Console.WriteLine(item);
+}
