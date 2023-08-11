3afb7043573e6feb181f16ebac3d0265df9dcef2
Aleksey Zhalnin <zhalninreg@me.com>
2023-08-11 19:09:38 +0300

    Added GetMax and Sort

--- BinaryHeap/Models/Heap.cs
+++ BinaryHeap/Models/Heap.cs
@@ -1,9 +1,22 @@
-﻿namespace BinaryHeap.Models;
+﻿using System.Collections;
 
-internal class Heap<T> where T : IComparable<T>
+namespace BinaryHeap.Models;
+
+internal class Heap<T>: IEnumerable<T> where T : IComparable<T>
 {
     private List<T> _items = new List<T>();
     public int Count => _items.Count;
+
+    public Heap()
+    {
+
+    }
+
+    public Heap(List<T> items)
+    {
+        _items.AddRange(items);
+        Sort(0);
+    }
 
     public T Peek()
     {
@@ -19,7 +32,7 @@ internal class Heap<T> where T : IComparable<T>
     {
         _items.Add(item);
 
-        var currentIndex = Count -1;//48.08
+        var currentIndex = Count -1;
         var parentIndex = GetParentIndex(currentIndex);
 
         while (currentIndex > 0 && _items[parentIndex].CompareTo(_items[currentIndex]) < 0)
@@ -31,8 +44,63 @@ internal class Heap<T> where T : IComparable<T>
         }
     }
 
+    public T GetMax()
+    {
+        var result = _items[0];
+        _items[0] = _items[Count - 1];
+        _items.RemoveAt(Count - 1);
+        Sort(0);
+        return result;
+    }
+
+    private void Sort(int currentIndex)
+    {
+        int leftIndex;
+        int rightIndex;
+        int maxIndex = currentIndex;
+
+        while (currentIndex < Count)
+        {
+            leftIndex = GetLeftChildIndex(currentIndex);
+            rightIndex = GetRightChildIndex(currentIndex);
+
+            if(leftIndex < Count && _items[leftIndex].CompareTo(_items[maxIndex]) > 0)
+            {
+                maxIndex = leftIndex;
+            }
+
+            if (rightIndex < Count && _items[rightIndex].CompareTo(_items[maxIndex]) > 0 )
+            {
+                maxIndex = rightIndex;
+            }
+
+            if(maxIndex == currentIndex)
+            {
+                break;
+            }
+
+            Swap(currentIndex, maxIndex);
+            currentIndex = maxIndex;
+        }
+    }
+
     private void Swap(int currentIndex, int parentIndex) =>
         (_items[currentIndex], _items[parentIndex]) = (_items[parentIndex], _items[currentIndex]);
 
+    private int GetLeftChildIndex(int currentIndex) => 2 * currentIndex + 1;
+
+    private int GetRightChildIndex(int currentIndex) => 2 * currentIndex + 2;
+
     private int GetParentIndex(int currentIndex) => (currentIndex - 1) / 2;
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        //return _items.GetEnumerator();
+        while (Count > 0)
+        {
+            yield return GetMax();
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
--- BinaryHeap/Program.cs
+++ BinaryHeap/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BinaryHeap.Models;
+
 Console.WriteLine("Hello, binary heap!");
+
+#region Static test
+//Heap<int> heap = new();
+//heap.Add(20);
+//heap.Add(11);
+//heap.Add(17);
+//heap.Add(7);
+//heap.Add(4);
+//heap.Add(13);
+//heap.Add(15);
+//heap.Add(14);
+#endregion
+
+var startItems = new List<int>();
+var rnd = new Random();
+for (int i = 0; i < 10; i++)
+{
+    startItems.Add(rnd.Next(-1000,1000));
+}
+
+Heap<int> heap = new(startItems);
+
+for (int i = 0; i < 10; i++)
+{
+    heap.Add(rnd.Next(-1000, 1000));
+}
+
+
+foreach (var item in heap)
+{
+    Console.WriteLine(item);
+}
